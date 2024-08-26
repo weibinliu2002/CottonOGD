48817beda7585f9cb4b1096167279afe722c4d06
weibinliu2002 <2102394487@qq.com>
2024-08-26 17:54:45 +0800

    2

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
+npm install NNNmodule --save-dev
+
 NNNmodule.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
 
   },
+};
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
 };
